0ed4dfb91968a114fa4e3737efdc122a2f4360fc
igorcolaco-byte <igor.colaco@escola.pr.gov.br>
2026-08-17 08:22:43 -0300

    3

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
-document.querySelector(".parametro-senha__texto");
+const umeroSenha = document.querySelector('.parametro-sennha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textcontent = tamanhoSenha;
 
-numeroSenha.textContent = 5;
+comst botoes = document.querySelectorAll('.parametro-senha__botao');
+botoes{0}.onlick = diminuiTamanho;
+botoes{1}.onclick = aumentaTamanho;
 
-cont botoes = document.querySelector('.')
+function diminuiTamanho() {
+    if (tamanhoSenha > 1) {tamanhoSenha = tamanhoSenha-1
+    }
+    numeroSenha.textcontent= tamanhoSenha
+}
+
+function aumentaTamanho(){
+    if (tamanhoSenha < 20) {
+        tamanhoSenha = tamanhoSenha+1;
+    }
+    numeroSenha.textcontent = tamanhoSenha;
+    gereSenha();
+}
+const campoSenha = docukment.querySelector('#campo-senha');
+const checkbox = document.querySelector('.checkbox');
+
+console.log(checkbox[0]checked);
+
+const letrasMaiusculasn = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
